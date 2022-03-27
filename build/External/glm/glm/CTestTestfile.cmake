ec7508069ced906411a565971de9c9b053fe27f7
Lixiyao-meow <lixiyao@Meow-MacBook-Air.local>
2022-03-27 20:54:51 +0200

    delete files

--- build/External/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xiyao/Documents/INF584/02_MyRenderer/External/glm/glm
-# Build directory: /home/xiyao/Documents/INF584/02_MyRenderer/build/External/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
